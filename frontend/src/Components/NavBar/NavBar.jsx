46807000cc740d02fa22be4fde5b5868d29fc7db
diadiaspora <dia.diaspora@gmail.com>
2025-07-25 19:00:35 -0400

    responsive maybe home

--- frontend/src/Components/NavBar/NavBar.jsx
+++ frontend/src/Components/NavBar/NavBar.jsx
@@ -1,84 +1,96 @@
 import { NavLink, Link } from "react-router";
+import { useState } from "react";
 import { logOut } from "../../services/authService";
+import { FaBars, FaTimes } from "react-icons/fa";
 import "./NavBar.css";
 import { FaRegCircleUser } from "react-icons/fa6";
+
 export default function NavBar({ user, setUser, profile }) {
-  // const navigate = useNavigate();
+  const [menuOpen, setMenuOpen] = useState(false);
 
   function handleLogOut() {
     logOut();
     setUser(null);
   }
-  console.log({ profile });
+
+  const toggleMenu = () => setMenuOpen(!menuOpen);
+  const closeMenu = () => setMenuOpen(false);
   const avatarSrc = profile?.avatar || "https://i.ibb.co/5x5Td7ks/av-1.png";
 
   return (
     <nav className="NavBar">
-      <NavLink to="/">
-        <img
-          src="/dawgmoney.png"
-          className="dogmoney"
-          alt="dog logo with money eyes"
-        ></img>
-        <img src="/dawg.png" className="doglogo" alt="dawg logo letters"></img>
-      </NavLink>
-      &nbsp; | &nbsp;
-      {user ? (
-        <>
-          <NavLink to="/fly">Flights</NavLink>
-          &nbsp; | &nbsp;
-          <NavLink to="/plans">Plan</NavLink>
-          &nbsp; | &nbsp;
-          <NavLink to="/write">Write</NavLink>
-          &nbsp; | &nbsp;
-          <NavLink to="/marketplace">Shop</NavLink>
-          &nbsp; | &nbsp;
-          <NavLink to="/profiles/">Profile</NavLink>
-          &nbsp; | &nbsp;
-          <Link to="/" onClick={handleLogOut}>
-            Log Out
-          </Link>
-          {/* <span>Welcome, {user.name}</span> */}
-          <span>
+      <div className="logo-and-toggle">
+        <NavLink to="/" onClick={closeMenu}>
+          <img src="/dawgmoney.png" className="dogmoney" alt="dog logo" />
+          <img src="/dawg.png" className="doglogo" alt="dawg logo" />
+        </NavLink>
+        <button className="menu-toggle" onClick={toggleMenu}>
+          {menuOpen ? <FaTimes /> : <FaBars />}
+        </button>
+      </div>
+
+      <div className={`nav-links ${menuOpen ? "active" : ""}`}>
+        {user ? (
+          <>
+            <NavLink to="/fly" onClick={closeMenu}>
+              Flights
+            </NavLink>
+            <NavLink to="/plans" onClick={closeMenu}>
+              Plan
+            </NavLink>
+            <NavLink to="/write" onClick={closeMenu}>
+              Write
+            </NavLink>
+            <NavLink to="/marketplace" onClick={closeMenu}>
+              Shop
+            </NavLink>
+            <NavLink to="/profiles/" onClick={closeMenu}>
+              Profile
+            </NavLink>
+            <Link
+              to="/"
+              onClick={() => {
+                handleLogOut();
+                closeMenu();
+              }}
+            >
+              Log Out
+            </Link>
             <img
-              src={profile?.avatar || <FaRegCircleUser />}
+              src={avatarSrc}
               className="avatar"
               alt="User Avatar"
               style={{ width: "33px", borderRadius: "300px" }}
             />
-          </span>
-        </>
-      ) : (
-        <>
-          <NavLink to="/fly" end>
-            Flights
-          </NavLink>
-          &nbsp; | &nbsp;
-          <NavLink to="/posts">Posts</NavLink>
-          &nbsp; | &nbsp;
-          <NavLink to="/blogs">Blogs</NavLink>
-          &nbsp; | &nbsp;
-          <NavLink to="/marketplace">Shop</NavLink>
-          &nbsp; | &nbsp;
-          <NavLink to="/login">Log In</NavLink>
-          &nbsp; | &nbsp;
-          <NavLink to="/signup">Sign Up</NavLink>
-          <span>
-            {/* <img
-              src={profile?.avatar || <FaRegCircleUser />}
-              className="avatar"
-              alt="User Avatar"
-              style={{ width: "33px" }}
-            /> */}
+          </>
+        ) : (
+          <>
+            <NavLink to="/fly" onClick={closeMenu}>
+              Flights
+            </NavLink>
+            <NavLink to="/posts" onClick={closeMenu}>
+              Posts
+            </NavLink>
+            <NavLink to="/blogs" onClick={closeMenu}>
+              Blogs
+            </NavLink>
+            <NavLink to="/marketplace" onClick={closeMenu}>
+              Shop
+            </NavLink>
+            <NavLink to="/login" onClick={closeMenu}>
+              Log In
+            </NavLink>
+            <NavLink to="/signup" onClick={closeMenu}>
+              Sign Up
+            </NavLink>
             <img
               src="https://i.ibb.co/bgQM3cM2/Vector.png"
               alt="Vector"
-              border="0"
-              style={{ marginRight: "42px" }}
+              className="avatar"
             />
-          </span>
-        </>
-      )}
+          </>
+        )}
+      </div>
     </nav>
   );
 }
