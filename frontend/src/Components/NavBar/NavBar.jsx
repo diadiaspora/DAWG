bbfdfcdce262ac84c68803a1bca5d12088d427d7
diadiaspora <dia.diaspora@gmail.com>
2025-07-26 17:59:31 -0400

    return

--- frontend/src/Components/NavBar/NavBar.jsx
+++ frontend/src/Components/NavBar/NavBar.jsx
@@ -14,79 +14,72 @@ export default function NavBar({ user, setUser, profile }) {
   const avatarSrc = profile?.avatar || "https://i.ibb.co/5x5Td7ks/av-1.png";
 
   return (
-    <nav>
-      <div className="NavBar">
-        <NavLink to="/">
-          <img
-            src="/dawgmoney.png"
-            className="dogmoney"
-            alt="dog logo with money eyes"
-          ></img>
-          <img
-            src="/dawg.png"
-            className="doglogo"
-            alt="dawg logo letters"
-          ></img>
-        </NavLink>
-        &nbsp; | &nbsp;
-        {user ? (
-          <>
-            <div id="destinations">
-              <NavLink to="/fly">Flights</NavLink>|
-              <NavLink to="/plans">Plan</NavLink>|
-              <NavLink to="/write">Write</NavLink>|
-              <NavLink to="/marketplace">Shop</NavLink>|
-              <NavLink to="/profiles/">Profile</NavLink>|
-              <Link to="/" onClick={handleLogOut}>
-                Log Out
-              </Link>
-              {/* <span>Welcome, {user.name}</span> */}
-              <span>
-                <img
-                  src={profile?.avatar || <FaRegCircleUser />}
-                  className="avatar"
-                  alt="User Avatar"
-                  style={{ width: "33px", borderRadius: "300px" }}
-                />
-              </span>
-            </div>
-          </>
-        ) : (
-          <>
-            <div id="destinations">
-              <NavLink to="/fly" end>
-                Flights
-              </NavLink>
-              |<NavLink to="/posts">Posts</NavLink>|
-              <NavLink to="/blogs">Blogs</NavLink>|
-              <NavLink to="/marketplace">Shop</NavLink>|
-              <NavLink to="/login">Log In</NavLink>|
-              <NavLink to="/signup">Sign Up</NavLink>
-              <span>
-                {/* <img
+    <nav className="NavBar">
+      <NavLink to="/">
+        <img
+          src="/dawgmoney.png"
+          className="dogmoney"
+          alt="dog logo with money eyes"
+        ></img>
+        <img src="/dawg.png" className="doglogo" alt="dawg logo letters"></img>
+      </NavLink>
+      &nbsp; | &nbsp;
+      {user ? (
+        <>
+          <NavLink to="/fly">Flights</NavLink>
+          &nbsp; | &nbsp;
+          <NavLink to="/plans">Plan</NavLink>
+          &nbsp; | &nbsp;
+          <NavLink to="/write">Write</NavLink>
+          &nbsp; | &nbsp;
+          <NavLink to="/marketplace">Shop</NavLink>
+          &nbsp; | &nbsp;
+          <NavLink to="/profiles/">Profile</NavLink>
+          &nbsp; | &nbsp;
+          <Link to="/" onClick={handleLogOut}>
+            Log Out
+          </Link>
+          {/* <span>Welcome, {user.name}</span> */}
+          <span>
+            <img
+              src={profile?.avatar || <FaRegCircleUser />}
+              className="avatar"
+              alt="User Avatar"
+              style={{ width: "33px", borderRadius: "300px" }}
+            />
+          </span>
+        </>
+      ) : (
+        <>
+          <NavLink to="/fly" end>
+            Flights
+          </NavLink>
+          &nbsp; | &nbsp;
+          <NavLink to="/posts">Posts</NavLink>
+          &nbsp; | &nbsp;
+          <NavLink to="/blogs">Blogs</NavLink>
+          &nbsp; | &nbsp;
+          <NavLink to="/marketplace">Shop</NavLink>
+          &nbsp; | &nbsp;
+          <NavLink to="/login">Log In</NavLink>
+          &nbsp; | &nbsp;
+          <NavLink to="/signup">Sign Up</NavLink>
+          <span>
+            {/* <img
               src={profile?.avatar || <FaRegCircleUser />}
               className="avatar"
               alt="User Avatar"
               style={{ width: "33px" }}
             /> */}
-                <img
-                  src="https://i.ibb.co/bgQM3cM2/Vector.png"
-                  alt="Vector"
-                  border="0"
-                  style={{ marginRight: "42px" }}
-                />
-              </span>
-            </div>
-          </>
-        )}
-      </div>
-
-
-      <div id="destinations-mobile">
-        <p>🍔</p>
-      </div>
-
-      
+            <img
+              src="https://i.ibb.co/bgQM3cM2/Vector.png"
+              alt="Vector"
+              border="0"
+              style={{ marginRight: "42px" }}
+            />
+          </span>
+        </>
+      )}
     </nav>
   );
 }
