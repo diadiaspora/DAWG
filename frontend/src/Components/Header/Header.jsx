7d21731e04b98533436e1384f3e4c5d7d4191883
diadiaspora <dia.diaspora@gmail.com>
2025-08-31 22:01:15 -0400

    images and headings

--- frontend/src/Components/Header/Header.jsx
+++ frontend/src/Components/Header/Header.jsx
@@ -5,20 +5,44 @@ export default function Header({ user }) {
     <div className="header">
       {user ? (
         <div className="signed-in">
-       
           <h1 style={{ fontSize: "22px", marginTop: "40px" }}>
             Hey {user.name}
           </h1>
-            <h2>Where are you and your dog going next?</h2>
-            
+          <h2>Where are you and your dog going next?</h2>
         </div>
       ) : (
-          <div className="signed-out">
-           
-          <h1 className="start">Hey </h1>
-          <h2>Where are you and your dog going next?</h2>
-           
-            </div>
+        <div style={{ display: "flex" }}>
+          <div>
+            <h1>Hey </h1>
+            <h2>Where are you and your dog going next?</h2>
+          </div>
+          <div>
+            <img src="/lab.png" className="dog" alt="labrador" />
+          </div>
+          <div>
+            <img src="/shitzu.png" className="dog" alt="shitzu" />
+          </div>
+          <div>
+            <img
+              src="/terrier.png"
+              className="dog"
+              alt="terrier"
+              style={{ marginTop: "24px" }}
+            />
+          </div>
+          <div>
+            <img src="/pit.png" className="dog" alt="pit" />
+          </div>
+          <div>
+            <img src="/chihuahua.png" className="dog" alt="chihuahua" />
+          </div>
+          <div>
+            <img src="/doodle.png" className="dog" alt="labradoodle" />
+          </div>
+          <div>
+            <img src="/english.png" className="dog" alt="labradoodle" />
+          </div>
+        </div>
       )}
     </div>
   );
